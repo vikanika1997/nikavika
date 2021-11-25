12f0dde6f0942739036aed7a9a0d5363d97f363e
vikanika1997 <91012977+vikanika1997@users.noreply.github.com>
2021-11-25 20:58:41 +0300

    Module 17

---
+++ module 17.py
@@ -0,0 +1,44 @@
+numbers = input('Введите последовательность чисел')
+a = int(input('Введите число'))
+numb = numbers.split()
+array = list(map(int, numb))
+print(array)
+
+
+def qsort(array, left=0, right=len(array)-1):
+    middle = (left + right) // 2
+
+    p = array[middle]
+    i, j = left, right
+    while i <= j:
+        while array[i] < p:
+            i += 1
+        while array[j] > p:
+            j -= 1
+        if i <= j:
+            array[i], array[j] = array[j], array[i]
+            i += 1
+            j -= 1
+
+    if j > left:
+        qsort(array, left, j)
+    if right > i:
+        qsort(array, i, right)
+qsort(array)
+print(array)
+
+
+def binary_search(array,element, left=0, right=len(array)-1):
+    if left > right:  # если левая граница превысила правую,
+        return False  # значит элемент отсутствует
+
+    middle = (right + left) // 2  # находимо середину
+    if array[middle] == element:  # если элемент в середине,
+        return middle  # возвращаем этот индекс
+    elif a < array[middle]:  # если элемент меньше элемента в середине
+        # рекурсивно ищем в левой половине
+        return binary_search(array, element, left, middle - 1)
+    else:  # иначе в правой
+        return binary_search(array, element, middle + 1, right)
+element = a
+print(binary_search(array, a))
